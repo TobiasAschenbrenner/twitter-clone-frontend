967bc29f42ca6142b6eebf5128e5e6b74321f535
Tobias Aschenbrenner <tobias_aschenbrenner@t-online.de>
2023-04-09 18:43:21 +0200

    feat: Update Profile route with username parameter

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           }
         />
         <Route
-          path="/profile"
+          path=":username"
           element={
             <ProtectedRoute>
               <Profile />
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,10 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./Sidebar.css";
 import CreateTweet from "../CreateTweet/CreateTweet";
 
 function Sidebar() {
   const [isCreatingTweet, setIsCreatingTweet] = useState(false);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    const fetchUsername = async () => {
+      try {
+        const token = localStorage.getItem("jwt");
+        const response = await fetch(
+          "https://api.chirp.koenidv.de/v1/user/me",
+          {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        );
+        const data = await response.json();
+        setUsername(data.username);
+      } catch (error) {
+        console.error("Error fetching username: ", error);
+      }
+    };
+
+    fetchUsername();
+  }, []);
 
   const handleTweetButtonClick = (e) => {
     e.preventDefault();
@@ -25,7 +49,7 @@ function Sidebar() {
             <Link to="/messages">Messages</Link>
           </li>
           <li className="sidebar-link">
-            <Link to="/profile">Profile</Link>
+            <Link to={`/${username}`}>Profile</Link>
           </li>
           <li className="sidebar-link">
             <Link to="/more">More</Link>
