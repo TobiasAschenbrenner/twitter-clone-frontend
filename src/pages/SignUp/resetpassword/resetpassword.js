04928e39d378c1fbeef32ab009c28235a432acc5
Florian König <koenidv@gmail.com>
2023-04-12 01:05:08 +0200

    Reset password page

--- src/pages/SignUp/resetpassword/resetpassword.js
+++ src/pages/SignUp/resetpassword/resetpassword.js
@@ -1,10 +1,13 @@
 import EmailForm from "./emailform";
+import PasswordForm from "./passwordform";
+import { useSearchParams } from "react-router-dom";
 
 const ResetPassword = () => {
-  const token = undefined;
+  const [searchParams] = useSearchParams();
+  const token = searchParams.get("token");
 
   if (token) {
-    return <></>;
+    return <PasswordForm token={token} />;
   } else {
     return <EmailForm />;
   }
