248da2535c000e14bfb20e77770c408842676392
Tobias Aschenbrenner <tobias_aschenbrenner@t-online.de>
2023-04-12 16:35:24 +0200

    feat: Implement dynamic Follow/Following/Unfollow button with hover effect and API integration

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./Profile.css";
 
 const API_BASE_URL = "https://api.chirp.koenidv.de";
@@ -70,10 +70,15 @@ const Profile = ({ userProfile, updateUserProfile }) => {
   const [updatedDisplayName, setUpdatedDisplayName] = useState(
     userProfile.displayname || ""
   );
-  const [updatedUsername, setUpdatedUsername] = useState(
-    userProfile.username || ""
-  );
+  const [updatedUsername, setUpdatedUsername] = useState("");
+
+  useEffect(() => {
+    setUpdatedUsername(userProfile.username || "");
+  }, [userProfile]);
+
   const [loggedInUsername, setLoggedInUsername] = useState(null);
+  const [following, setFollowing] = useState(false);
+  const [hover, setHover] = useState(false);
 
   useEffect(() => {
     const fetchUsername = async () => {
@@ -85,6 +90,7 @@ const Profile = ({ userProfile, updateUserProfile }) => {
   }, []);
 
   const isCurrentUser = loggedInUsername === userProfile.username;
+
   const handleFollow = async () => {
     try {
       const ref_id = await getUserId(userProfile.username);
@@ -98,15 +104,16 @@ const Profile = ({ userProfile, updateUserProfile }) => {
               "Content-Type": "application/json",
               Authorization: `Bearer ${localStorage.getItem("jwt")}`,
             },
-            body: JSON.stringify({ follow: true }), // Updated here
+            body: JSON.stringify({ follow: !following }),
           }
         );
 
         if (response.ok) {
-          console.log("Follow successful");
+          console.log("Follow/Unfollow successful");
+          setFollowing(!following);
         } else {
           console.error(
-            "Error following user:",
+            "Error following/unfollowing user:",
             response.status,
             response.statusText
           );
@@ -115,73 +122,94 @@ const Profile = ({ userProfile, updateUserProfile }) => {
         console.error("Error: user_id or ref_id is null");
       }
     } catch (error) {
-      console.error("Error following user:", error);
+      console.error("Error following/unfollowing user:", error);
     }
   };
 
-  const handleEdit = () => {
-    setEditing(true);
-  };
+  const checkFollowStatus = useCallback(async () => {
+    try {
+      const ref_id = await getUserId(userProfile.username);
 
-  const handleSave = async () => {
-    updateUserProfile({
-      ...userProfile,
-      displayname: updatedDisplayName,
-      username: updatedUsername,
-      bio: updatedBio,
-    });
-    setEditing(false);
-  };
+      if (ref_id) {
+        const response = await fetch(
+          `${API_BASE_URL}/v1/user/${ref_id}/follow`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+            },
+          }
+        );
 
-  const formatDate = (dateString) => {
-    const date = new Date(dateString);
-    const month = date.toLocaleString("default", { month: "long" });
-    const year = date.getFullYear();
-    return `${month} ${year}`;
-  };
+        if (response.ok) {
+          const data = await response.json();
+          setFollowing(data.following);
+        } else {
+          console.error(
+            "Error checking follow status:",
+            response.status,
+            response.statusText
+          );
+        }
+      } else {
+        console.error("Error: user_id or ref_id is null");
+      }
+    } catch (error) {
+      console.error("Error checking follow status:", error);
+    }
+  }, [userProfile.username]);
+
+  useEffect(() => {
+    if (!isCurrentUser) {
+      checkFollowStatus();
+    }
+  }, [loggedInUsername, userProfile, checkFollowStatus, isCurrentUser]);
 
   return (
     <div className="profile">
-      <div>
-        {editing && isCurrentUser ? (
-          <>
-            <label htmlFor="updatedDisplayName">Display Name:</label>
-            <input
-              type="text"
-              id="updatedDisplayName"
-              value={updatedDisplayName}
-              onChange={(e) => setUpdatedDisplayName(e.target.value)}
-            />
-            <label htmlFor="updatedUsername">Username:</label>
-            <input
-              type="text"
-              id="updatedUsername"
-              value={updatedUsername}
-              onChange={(e) => setUpdatedUsername(e.target.value)}
-            />
-            <label htmlFor="updatedBio">Bio:</label>
-            <textarea
-              id="updatedBio"
-              value={updatedBio}
-              onChange={(e) => setUpdatedBio(e.target.value)}
-            />
-            <button onClick={handleSave}>Save Changes</button>
-          </>
-        ) : (
-          <>
-            <img src={userProfile.profile_image_url} alt="Profile" />
-            <h2>{userProfile.displayname}</h2>
-            <p>@{userProfile.username}</p>
-            <p>{userProfile.bio}</p>
-            {isCurrentUser ? (
-              <button onClick={handleEdit}>Edit Profile</button>
-            ) : (
-              <button onClick={handleFollow}>Follow</button>
+      <img src={userProfile.avatar_url} alt="Profile" />
+      {editing ? (
+        <>
+          <input
+            type="text"
+            value={updatedDisplayName}
+            onChange={(e) => setUpdatedDisplayName(e.target.value)}
+          />
+          <textarea
+            value={updatedBio}
+            onChange={(e) => setUpdatedBio(e.target.value)}
+          ></textarea>
+          <button
+            onClick={updateUserProfile(
+              updatedUsername,
+              updatedDisplayName,
+              updatedBio
             )}
-          </>
-        )}
-        <p>Joined: {formatDate(userProfile.created_at)}</p>
-      </div>
+          >
+            Save
+          </button>
+        </>
+      ) : (
+        <>
+          <h2>{userProfile.displayname}</h2>
+          <p>@{userProfile.username}</p>
+          <p>{userProfile.bio}</p>
+          {isCurrentUser ? (
+            <button onClick={() => setEditing(!editing)}>
+              {editing ? "Cancel" : "Edit"}
+            </button>
+          ) : (
+            <button
+              onClick={handleFollow}
+              onMouseEnter={() => setHover(true)}
+              onMouseLeave={() => setHover(false)}
+            >
+              {following ? (hover ? "Unfollow" : "Following") : "Follow"}
+            </button>
+          )}
+        </>
+      )}
     </div>
   );
 };
