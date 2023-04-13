d97d2d970a659f91d4aae111043e9d3ae6ac6c6a
Florian König <koenidv@gmail.com>
2023-04-13 18:33:53 +0200

    Don't query username in Sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,31 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Sidebar.scss";
 import CreateTweet from "../CreateTweet/CreateTweet";
 
 function Sidebar() {
   const [isCreatingTweet, setIsCreatingTweet] = useState(false);
-  const [username, setUsername] = useState("");
-
-  useEffect(() => {
-    const fetchUsername = async () => {
-      try {
-        const token = localStorage.getItem("jwt");
-        const response = await fetch("https://api.thechirp.de/v1/user/me", {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        const data = await response.json();
-        setUsername(data.username);
-      } catch (error) {
-        console.error("Error fetching username: ", error);
-      }
-    };
-
-    fetchUsername();
-  }, []);
 
   const handleTweetButtonClick = (e) => {
     e.preventDefault();
